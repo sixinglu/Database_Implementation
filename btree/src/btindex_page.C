25e4da098c0a2c091f508bcdc03109097970ef86
cynthialu <sixinglu@email.arizona.edu>
2016-04-08 18:20:44 -0700

    recover

--- btree/src/btindex_page.C
+++ btree/src/btindex_page.C
@@ -2,7 +2,7 @@
  * btindex_page.C - implementation of class BTIndexPage
  *
  * Johannes Gehrke & Gideon Glass  951016  CS564  UW-Madison
- * Edited by Young-K. Suh (yksuh@cs.arizona.edu) 03/27/14 CS560 Database Systems Implementation 
+ * Edited by Young-K. Suh (yksuh@cs.arizona.edu) 03/27/14 CS560 Database Systems Implementation
  */
 
 #include "btindex_page.h"
@@ -10,9 +10,9 @@
 
 // Define your Error Messge here
 const char* BTIndexErrorMsgs[] = {
-  //Possbile error messages,
-  //OK,
-  //Record Insertion Failure,
+    //Possbile error messages,
+    //OK,
+    //Record Insertion Failure,
 };
 
 static error_string_table btree_table(BTINDEXPAGE, BTIndexErrorMsgs);
@@ -22,24 +22,24 @@ Status BTIndexPage::insertKey (const void *key,
                                PageId pageNo,
                                RID& rid)
 {
-  // put your code here
-KeyDataEntry tmpEntry;
-int entry_len;
-Datatype dtype;
-dtype.pageNo = pageNo;
-nodetype ndtype = INDEX;
-make_entry(&tmpEntry,key_type,key,ndtype,dtype,
-                &entry_len);
-
-insertRecord (key_type, (char *)&tmpEntry, entry_len,rid);
-  return OK;
+    // put your code here
+    KeyDataEntry tmpEntry;
+    int entry_len;
+    Datatype dtype;
+    dtype.pageNo = pageNo;
+    nodetype ndtype = INDEX;
+    make_entry(&tmpEntry,key_type,key,ndtype,dtype,
+               &entry_len);
+    
+    insertRecord (key_type, (char *)&tmpEntry, entry_len,rid);
+    return OK;
 }
 
 Status BTIndexPage::deleteKey (const void *key, AttrType key_type, RID& curRid)
 {
-  // put your code here
-Status status;
-  RID currRID, prevRID;
+    // put your code here
+    Status status;
+    RID currRID, prevRID;
     status = this->firstRecord(currRID);               // read the first index RID
     do{
         KeyDataEntry recPtr;
@@ -55,12 +55,12 @@ Status status;
         
         // compare the key
         int compareResult = keyCompare(key,targetkey,key_type);
-        if(compareResult==0){  // the key already there, so delete it 
-		deleteRecord (currRID);
+        if(compareResult==0){  // the key already there, so delete it
+            deleteRecord (currRID);
             return OK;
         }
         else if(compareResult<0){  // the insert key is smaller
-				// don't want to delete it
+            // don't want to delete it
             return DONE;
         }
         // else the insert key is larger, move on to next key record
@@ -69,8 +69,8 @@ Status status;
         
     }
     while(this->nextRecord(prevRID,currRID)==OK);
-
-  return OK;
+    
+    return OK;
 }
 
 Status BTIndexPage::get_page_no(const void *key,
@@ -98,16 +98,12 @@ Status BTIndexPage::get_page_no(const void *key,
         Datatype *targetdata;
         get_key_data(targetkey, targetdata, &recPtr, recLen, INDEX); // must be index
         
-        
-        // if the target key is deleted
-        // ...
-        
         // compare the key
         int compareResult = keyCompare(key,targetkey,key_type);
-        if(compareResult==0){  // the key already there
-            return DONE;
-        }
-        else if(compareResult<0){  // the insert key is smaller
+//        if(compareResult==0){  // the key already there
+//            return DONE;
+//        }
+        if(compareResult<0){  // the insert key is smaller
             
             // this record point to the page is the next level search page
             pageNo = targetdata->pageNo;
@@ -129,12 +125,12 @@ Status BTIndexPage::get_first(RID& rid,
                               void *key,
                               PageId & pageNo)
 {
-  // put your code here
-  return OK;
+    // put your code here
+    return OK;
 }
 
 Status BTIndexPage::get_next(RID& rid, void *key, PageId & pageNo)
 {
-  // put your code here
-  return OK;
+    // put your code here
+    return OK;
 }
